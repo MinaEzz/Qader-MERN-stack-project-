44f23c28162215115cdd6501dbc9b32806d943e1
Mina Ezz <minaezzat98@gmail.com>
2024-06-17 21:13:49 +0300

    addjob controller removed

--- backend/routes/job.routes.js
+++ backend/routes/job.routes.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const {addJob, getAllJobs} = require("../controller/job.controller")
+const {getAllJobs} = require("../controller/job.controller")
 
 router.route("/").get(getAllJobs)
 
-
-
-
-
 module.exports = router;
-
-
